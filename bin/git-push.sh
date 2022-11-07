0926af0d4a02917985bb367bf260bd89cc1f4ecd
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-06 23:23:22 -0800

    on-behalf-of simply doesn't work

--- bin/git-push.sh
+++ bin/git-push.sh
@@ -6,8 +6,6 @@ git add .
 if ! git diff-index --quiet HEAD --; then
   git config --global user.email "noreply@github.com"
   git config --global user.name "GitHub"
-  git commit -m "GitHub Actions: ${message}
-
-on-behalf-of: @rubybench <noreply@github.com>"
+  git commit -m "GitHub Actions: ${message}"
   git push origin master
 fi
