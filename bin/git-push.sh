f0556fafb96857e3b3b002ebfabbd22639c1e785
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-06 23:17:08 -0800

    Insert a newline

--- bin/git-push.sh
+++ bin/git-push.sh
@@ -7,6 +7,7 @@ if ! git diff-index --quiet HEAD --; then
   git config --global user.email "noreply@github.com"
   git config --global user.name "GitHub"
   git commit -m "GitHub Actions: ${message}
-on-behalf-of: @rubybench takashikkbn@gmail.com"
+
+on-behalf-of: @rubybench noreply@github.com"
   git push origin master
 fi
