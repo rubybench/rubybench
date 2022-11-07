c5ee81e26ef8667f41aa504b7936c44efc220aad
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-06 23:39:36 -0800

    Implement benchmark runner

--- bin/benchmark.rb
+++ bin/benchmark.rb
@@ -1,19 +1,64 @@
 #!/usr/bin/env ruby
+require 'yaml'
+require 'json'
 
-# 1. Pick a not-too-old date when there's no result yet
-# TODO: build a yaml file that lists available versions
-date = 20221106
+benchmark = ARGV.first
+if benchmark.nil?
+  abort "Usage: #{$0} BENCHMARK"
+end
 
-# 2. Run yjit-bench and capture the result
-# TODO: use timeout
-cmd = [
-  'docker', 'run', '--privileged',
+# Load past benchmark results
+if File.exist?("results/#{benchmark}.yml")
+  results = YAML.load_file("results/#{benchmark}.yml")
+else
+  results = {}
+end
+
+# Find a Ruby that has not been benchmarked yet
+target_dates = YAML.load_file('rubies.yml').keys.reverse
+target_date = target_dates.find do |date|
+  !results.key?(date)
+end
+if target_date.nil?
+  puts "Every Ruby version is already benchmarked"
+  return
+end
+
+# Start a container
+system(
+  'docker', 'run', '-d', '--privileged', '--name', 'rubybench',
   '-v', "#{Dir.pwd}/yjit-bench:/yjit-bench",
-  "rubylang/ruby:master-nightly-#{date}-focal",
-  "bash", "-c",
-  "cd /yjit-bench && ./run_benchmarks.rb optcarrot -e 'ruby --yjit'",
-]
-out = IO.popen(cmd, &:read)
-puts out
+  "rubylang/ruby:master-nightly-#{target_date}-focal",
+  'bash', '-c', 'while true; do sleep 100000; done',
+  exception: true,
+)
+at_exit { system('docker', 'rm', '-f', 'rubybench', exception: true) }
 
-# 3. Insert the result and Ruby description
+# Prepare for running benchmarks
+# TODO: Install dependency for railsbench
+
+# Run benchmarks for VM, MJIT, and YJIT
+result = []
+timeout = 10 * 60 # 10min
+[nil, '--mjit', '--yjit'].each do |opts|
+  cmd = [
+    'timeout', timeout.to_s, 'docker', 'exec', 'rubybench',
+    'bash', '-c', "cd /yjit-bench && ./run_benchmarks.rb #{benchmark} -e 'ruby #{opts}'",
+  ]
+  out = IO.popen(cmd, &:read)
+  puts out
+  if $?.success?
+    line = out.lines.reverse.find { |line| line.start_with?(benchmark) }
+    result << Float(line.split(/\s+/)[1])
+  else
+    result << nil
+  end
+end
+results[target_date] = result
+
+# Update results/*.yml
+File.open("results/#{benchmark}.yml", "w") do |io|
+  results.sort_by(&:first).each do |date, times|
+    io.puts "#{date}: #{times.to_json}"
+  end
+end
