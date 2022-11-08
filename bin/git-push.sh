83d0948e53c4f3d4bf14438edb40b02014f517da
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-08 00:14:07 -0800

    Update git-push.sh for servers

--- bin/git-push.sh
+++ bin/git-push.sh
@@ -4,9 +4,11 @@ message="$@"
 set -x
 git add .
 if ! git diff-index --quiet HEAD --; then
-  git config --global user.email "noreply@github.com"
-  git config --global user.name "GitHub"
-  git commit -m "GitHub Actions: ${message}"
+  if [[ -n "$GITHUB_ACTION" ]]; then
+    git config --global user.email "noreply@github.com"
+    git config --global user.name "GitHub"
+  fi
+  git commit -m "$message"
   git pull --rebase origin master
   git push origin master
 fi
