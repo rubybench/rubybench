2db3e09405d5d58de7e4d97a8825e690f96cf414
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-08 00:21:11 -0800

    Add ruby-kai1.sh

---
+++ bin/ruby-kai1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euxo pipefail
+
+rubybench=$(cd $(dirname "$0"); cd ..; pwd)
+cd "$rubybench"
+
+# Refresh rubybench
+git reset --hard HEAD
+git checkout master
+git fetch origin
+git reset --hard origin/master
+
+# Run benchmarks
+for bench in optcarrot railsbench; do
+  bin/benchmark.rb "$bench"
+  bin/git-push.sh "ruby-kai1: Benchmark ${bench}"
+done
--- infra/recipes/default.rb
+++ infra/recipes/default.rb
@@ -1,4 +1,4 @@
 include_recipe 'docker'
 include_recipe 'rubybench'
 
-# 2. set up systemd timer
+# TODO: set up systemd timer
