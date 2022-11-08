8c4e732365719ec675c48817d268032dd77f2a06
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-08 00:11:09 -0800

    Fix gitconfig setup

--- infra/recipes/rubybench.rb
+++ infra/recipes/rubybench.rb
@@ -6,7 +6,9 @@ execute 'git clone --recursive https://github.com/rubybench/rubybench /home/k0ku
 end
 
 remote_file '/home/k0kubun/.gitconfig' do
-  user 'k0kubun'
+  owner 'k0kubun'
+  group 'k0kubun'
+  mode '644'
 end
 
 # $ ssh-keygen
