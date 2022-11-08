c0047d6e3f04f2e528fd8998d4a1f3322fe80650
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-08 00:09:55 -0800

    Configure git

--- infra/recipes/rubybench.rb
+++ infra/recipes/rubybench.rb
@@ -4,3 +4,10 @@ execute 'git clone --recursive https://github.com/rubybench/rubybench /home/k0ku
   user 'k0kubun'
   not_if 'test -d /home/k0kubun/rubybench'
 end
+
+remote_file '/home/k0kubun/.gitconfig' do
+  user 'k0kubun'
+end
+
+# $ ssh-keygen
+# Set it as a deploy key
