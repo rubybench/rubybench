7b72fc2f72a27e0855bafe67672be5713c70efbf
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-08 00:33:56 -0800

    Start and enable rubybench.timer

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -4,12 +4,6 @@ set -euxo pipefail
 rubybench=$(cd $(dirname "$0"); cd ..; pwd)
 cd "$rubybench"
 
-# Refresh rubybench
-git reset --hard HEAD
-git checkout master
-git fetch origin
-git reset --hard origin/master
-
 # Run benchmarks
 for bench in optcarrot railsbench; do
   bin/benchmark.rb "$bench"
--- infra/recipes/default.rb
+++ infra/recipes/default.rb
@@ -1,4 +1,18 @@
 include_recipe 'docker'
 include_recipe 'rubybench'
 
-# TODO: set up systemd timer
+remote_file '/lib/systemd/system/rubybench.service' do
+  mode '600'
+  owner 'root'
+  group 'root'
+end
+
+remote_file '/lib/systemd/system/rubybench.timer' do
+  mode '600'
+  owner 'root'
+  group 'root'
+end
+
+service 'rubybench.timer' do
+  action [:start, :enable]
+end
