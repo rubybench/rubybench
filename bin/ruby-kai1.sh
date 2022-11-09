be480f64a937b99a42121170114e3e2b73f22021
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-08 20:44:02 -0800

    Support more benchmarks

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -5,7 +5,8 @@ rubybench=$(cd $(dirname "$0"); cd ..; pwd)
 cd "$rubybench"
 
 # Run benchmarks
-for bench in optcarrot railsbench; do
+for bench in activerecord hexapdf liquid-render mail psych-load railsbench \
+             binarytrees chunky_png erubi erubi_rails etanni fannkuchredux lee nbody optcarrot ruby-lsp rubykon; do
   bin/benchmark.rb "$bench"
   bin/git-push.sh "ruby-kai1: Benchmark ${bench}"
 done
