61a8cf9fe70ac6324c0af04cae5a9c528bc9d598
GitHub <noreply@github.com>
2022-11-11 09:11:41 +0000

    ruby-kai1: Benchmark fannkuchredux
    ruby-kai1: Benchmark lee
    
    ruby-kai1: Benchmark nbody
    
    ruby-kai1: Benchmark ruby-lsp
    
    ruby-kai1: Benchmark ruby app_answer.rb

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -4,9 +4,17 @@ set -euxo pipefail
 rubybench=$(cd $(dirname "$0"); cd ..; pwd)
 cd "$rubybench"
 
+# Ruby ruby/ruby
+for bench in benchmark/ruby/benchmark/*.rb benchmark/ruby/benchmark/*.yml; do
+  bench="$(basename "$bench")"
+  benchmark/ruby.rb "$bench"
+  bin/git-push.sh "ruby-kai1: Benchmark ruby ${bench}"
+done
+exit
+
 # Run yjit-bench
 for bench in activerecord hexapdf liquid-render mail psych-load railsbench \
              binarytrees chunky_png erubi erubi_rails etanni fannkuchredux lee nbody optcarrot ruby-lsp rubykon; do
   benchmark/yjit-bench.rb "$bench"
-  bin/git-push.sh "ruby-kai1: Benchmark ${bench}"
+  bin/git-push.sh "ruby-kai1: Benchmark yjit-bench ${bench}"
 done
