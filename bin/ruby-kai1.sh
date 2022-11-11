2063cb8889ce9f04fd3695ac41cc3ee64c9339b2
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-11 00:18:10 -0800

    Move yjit-bench.rb to benchmark/

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -7,6 +7,6 @@ cd "$rubybench"
 # Run yjit-bench
 for bench in activerecord hexapdf liquid-render mail psych-load railsbench \
              binarytrees chunky_png erubi erubi_rails etanni fannkuchredux lee nbody optcarrot ruby-lsp rubykon; do
-  bin/yjit-bench.rb "$bench"
+  benchmark/yjit-bench.rb "$bench"
   bin/git-push.sh "ruby-kai1: Benchmark ${bench}"
 done
