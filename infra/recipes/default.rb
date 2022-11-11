83239fc2ec59a2a255de564b28e7c6e87b84db7f
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-10 23:46:42 -0800

    Update submodules

--- infra/recipes/default.rb
+++ infra/recipes/default.rb
@@ -2,16 +2,22 @@ include_recipe 'docker'
 include_recipe 'rubybench'
 include_recipe 'scaling_governor'
 
+execute 'systemctl daemon-reload' do
+  action :nothing
+end
+
 remote_file '/lib/systemd/system/rubybench.service' do
   mode '600'
   owner 'root'
   group 'root'
+  notifies :run, 'execute[systemctl daemon-reload]'
 end
 
 remote_file '/lib/systemd/system/rubybench.timer' do
   mode '600'
   owner 'root'
   group 'root'
+  notifies :run, 'execute[systemctl daemon-reload]'
 end
 
 service 'rubybench.timer' do
