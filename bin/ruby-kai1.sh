865c51d3caa27e83b1815dce69d4d6e00033394f
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-11 20:40:24 -0800

    Test run

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -5,15 +5,17 @@ rubybench=$(cd $(dirname "$0"); cd ..; pwd)
 cd "$rubybench"
 
 # Ruby ruby/ruby
-for bench in benchmark/ruby/benchmark/*.rb benchmark/ruby/benchmark/*.yml; do
+#for bench in benchmark/ruby/benchmark/*.rb benchmark/ruby/benchmark/*.yml; do
+for bench in benchmark/ruby/benchmark/app_answer.rb; do
   bench="$(basename "$bench")"
   benchmark/ruby.rb "$bench"
 done
 bin/git-push.sh "ruby-kai1: Benchmark ruby/ruby"
 
 # Run yjit-bench
-for bench in activerecord hexapdf liquid-render mail psych-load railsbench \
-             binarytrees chunky_png erubi erubi_rails etanni fannkuchredux lee nbody optcarrot ruby-lsp rubykon; do
+#for bench in activerecord hexapdf liquid-render mail psych-load railsbench \
+#             binarytrees chunky_png erubi erubi_rails etanni fannkuchredux lee nbody optcarrot ruby-lsp rubykon; do
+for bench in activerecord; do
   benchmark/yjit-bench.rb "$bench"
 done
 bin/git-push.sh "ruby-kai1: Benchmark yjit-bench"
