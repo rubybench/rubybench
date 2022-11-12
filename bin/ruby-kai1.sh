3703d54b66d8ab5c63c41bd2343ed75e0dbb7f1f
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-11 21:17:09 -0800

    Run yjit-bench first

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -4,16 +4,16 @@ set -euxo pipefail
 rubybench=$(cd $(dirname "$0"); cd ..; pwd)
 cd "$rubybench"
 
-# Ruby ruby/ruby
-for bench in benchmark/ruby/benchmark/*.rb benchmark/ruby/benchmark/*.yml; do
-  bench="$(basename "$bench")"
-  benchmark/ruby.rb "$bench"
-done
-bin/git-push.sh "ruby-kai1: Benchmark ruby/ruby"
-
 # Run yjit-bench
 for bench in activerecord hexapdf liquid-render mail psych-load railsbench \
              binarytrees chunky_png erubi erubi_rails etanni fannkuchredux lee nbody optcarrot ruby-lsp rubykon; do
   benchmark/yjit-bench.rb "$bench"
 done
 bin/git-push.sh "ruby-kai1: Benchmark yjit-bench"
+
+# Ruby ruby/ruby
+for bench in benchmark/ruby/benchmark/*.rb benchmark/ruby/benchmark/*.yml; do
+  bench="$(basename "$bench")"
+  benchmark/ruby.rb "$bench"
+done
+bin/git-push.sh "ruby-kai1: Benchmark ruby/ruby"
