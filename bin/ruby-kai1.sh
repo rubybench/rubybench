8aaf2db9b335e0391386ab4fb086d616718fed96
Takashi Kokubun <takashikkbn@gmail.com>
2023-04-06 00:59:30 -0700

    Rename setivar_young

--- bin/ruby-kai1.sh
+++ bin/ruby-kai1.sh
@@ -5,9 +5,9 @@ rubybench=$(cd $(dirname "$0"); cd ..; pwd)
 cd "$rubybench"
 
 # Run yjit-bench
-for bench in activerecord hexapdf liquid-render mail psych-load railsbench ruby-lsp sequel liquid-c \
-             binarytrees chunky_png erubi erubi_rails etanni fannkuchredux ruby-json lee nbody optcarrot rubykon \
-             30k_ifelse 30k_methods cfunc_itself fib getivar keyword_args respond_to setivar setivar_object setivar_young_object str_concat throw; do
+for bench in activerecord erubi_rails hexapdf liquid-c liquid-render mail psych-load railsbench ruby-lsp sequel \
+             binarytrees chunky_png erubi etanni fannkuchredux ruby-json lee nbody optcarrot rubykon \
+             30k_ifelse 30k_methods cfunc_itself fib getivar keyword_args respond_to setivar setivar_object setivar_young str_concat throw; do
   benchmark/yjit-bench.rb "$bench"
 done
 bin/dashboard.rb
