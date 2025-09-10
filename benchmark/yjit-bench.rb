fb93b37572777caf6a744f861b91e3a81e97e48f
Takashi Kokubun <takashikkbn@gmail.com>
2025-09-09 20:36:58 -0700

    Add missing sudo for yjit-bench

--- benchmark/yjit-bench.rb
+++ benchmark/yjit-bench.rb
@@ -84,7 +84,7 @@ class YJITBench
         'bash', '-c', 'while true; do sleep 100000; done',
         exception: true,
       )
-      cmd = 'apt-get update && apt install -y build-essential git libsqlite3-dev libyaml-dev pkg-config xz-utils'
+      cmd = 'apt-get update && apt install -y build-essential git libsqlite3-dev libyaml-dev pkg-config xz-utils sudo'
       system('docker', 'exec', container, 'bash', '-c', cmd, exception: true)
       @started_containers << container
     end
