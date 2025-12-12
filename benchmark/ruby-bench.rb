875c9e9194c380bea4a9a7f2300aeaad0c7d91e0
Matt Valentine-House <matt@eightbitraptor.com>
2025-12-12 17:39:57 +0000

    Introduce RubyBench::LocalRunner
    This will run the ruby-bench suite with a Ruby binary specified on the
    command line

--- benchmark/ruby-bench.rb
+++ benchmark/ruby-bench.rb
@@ -11,11 +11,31 @@ OptionParser.new do |parser|
   parser.on("--results-root [ROOT]", "A directory in which the results tree will be output") do |path|
     options[:results_root] = path
   end
+  parser.on("--ruby PATH", "Path to Ruby binary (bypasses Docker)") do |path|
+    options[:ruby_binary] = path
+  end
+  parser.on("--date DATE", "Target date for results (required with --ruby, format: YYYYMMDD)") do |date|
+    options[:target_date] = date.to_i
+  end
 end.parse!
 
-runner = RubyBench::DockerRunner.new(rubies_path: RUBIES_PATH)
-ruby_bench = RubyBench.new(runner: runner, results_root: options[:results_root])
+if options[:ruby_binary] && !options[:target_date]
+  abort "ERROR: --date is required when using --ruby"
+end
+if options[:target_date] && !options[:ruby_binary]
+  abort "ERROR: --ruby is required when using --date"
+end
 
+runner = if options[:ruby_binary]
+  RubyBench::LocalRunner.new(ruby_binary: options[:ruby_binary], target_date: options[:target_date])
+else
+  unless File.exist?(RUBIES_PATH)
+    abort "ERROR: rubies.yml not found at #{RUBIES_PATH}. Please setup using bin/prepare-results.rb"
+  end
+  RubyBench::DockerRunner.new(rubies_path: RUBIES_PATH)
+end
+
+ruby_bench = RubyBench.new(runner: runner, results_root: options[:results_root])
 at_exit { ruby_bench.shutdown }
 
 if ARGV.empty?
